89b11dd7a6ad21d3315c26352c564a4d3b824fcc
Joana Custodio <joana@MacBook13Home.local>
2025-05-17 11:57:23 -0300

    primeiro commit

---
+++ primeiro-hello/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Net.Security;
+
+Console.WriteLine("Hello, World!");
+int a = 10;
+int b = 2;
+Console.WriteLine("A soma é: {0}", a + b);
+Console.WriteLine("A é {0} e B é {1}", a, b);
+
+Console.WriteLine("outra forma é A é " + b + " e B é " + b);
